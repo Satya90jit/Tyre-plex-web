a19641a37b14f7850c88e195660359acf46834a4
Satya90jit <satyajitsahu2002@gmail.com>
2024-08-28 01:25:36 +0530

    UI update

--- src/components/home/QueryCard.tsx
+++ src/components/home/QueryCard.tsx
@@ -4,7 +4,7 @@ import React from "react";
 const QueryCard = () => {
   return (
     <section>
-      <div className="px-8 rounded-lg py-10 flex lg:flex-row flex-col justify-between lg:gap-60 gap-12 w-full bg-red-400/40 my-16">
+      <div className="px-8 rounded-lg py-10 flex lg:flex-row flex-col justify-between lg:gap-60 gap-12 w-full bg-slate-300/20 my-16">
         <div className="flex lg:flex-row flex-col gap-5">
           <div className="lg:flex justify-start hidden">
             <span className="flex items-center justify-center lg:p-8 p-8 rounded-full text-white bg-gray-800/40">
@@ -21,19 +21,21 @@ const QueryCard = () => {
             </p>
           </div>
         </div>
-        <div className="md:p-6 p-3 bg-red-900/30 shadow-md rounded-lg lg:w-[45rem] w-full border border-slate-100">
+        <div className="md:p-6 p-3 bg-slate-700/20 shadow-md rounded-lg lg:w-[45rem] w-full border border-slate-100">
           <div className="flex flex-col space-y-4">
             <div className="flex items-center">
               <input
                 type="text"
                 placeholder="Your Query"
-                className="focus:outline-none bg-gray-900/20 p-2 h-full text-sm text-white w-full flex-grow rounded-l-lg"
+                className="focus:outline-none bg-gray-700 p-2 h-full text-sm text-white w-full flex-grow rounded-l-lg"
               />
-              <span className="bg-orange-800 common-transition hover:bg-orange-700 h-full inline-block text-white text-sm py-1 px-2 rounded-r-lg">
+              <span className="bg-orange-800 cursor-pointer common-transition hover:bg-orange-700 h-full inline-block text-white text-sm py-1 px-2 rounded-r-lg">
                 <Send />
               </span>
             </div>
-            <p className="text-white">I agree to the Terms and Conditions</p>
+            <p className="text-gray-900">
+              Submit your query, and we'll get back to you shortly.
+            </p>
           </div>
         </div>
       </div>
